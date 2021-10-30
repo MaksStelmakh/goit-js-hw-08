d6544c460afe3670b7eeb6e3e4dcee2d9a55c6e5
MaksStelmakh <84736957+MaksStelmakh@users.noreply.github.com>
2021-10-30 10:04:58 +0000

    Deploying to gh-pages from @ MaksStelmakh/goit-js-hw-08@785e7a0bd96eb0a4a60af2797783f715b2b11bd4 🚀

--- 02-video.12780038.js
+++ 02-video.12780038.js
@@ -7,4 +7,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=r(require("@vimeo/player")),t=r(require("lodash.throttle"));function r(e){return e&&e.__esModule?e:{default:e}}const o=document.querySelector("iframe"),u=new Vimeo.Player(o);function i(e){localStorage.setItem("videoplayer-current-time",e.seconds)}function l(){let e=localStorage.getItem("videoplayer-current-time");u.setCurrentTime(e)}l(),u.on("timeupdate",(0,t.default)(i,1e3));
 },{"@vimeo/player":"zU8P","lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/goit-js-hw-08/02-video.60079021.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.12780038.js.map
